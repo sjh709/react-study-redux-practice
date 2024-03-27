2673e7a7c0b18fe68de0a17f19559b267fc71c38
Jihee <sjh709@naver.com>
2024-03-27 22:47:12 +0900

    증가 카운트 구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increase = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='area'>
+      <div className='wrapper'>
+        <div className='number'>{count}</div>
+        <button className='btn-push' onClick={increase}>
+          증가
+        </button>
+      </div>
     </div>
   );
 }
